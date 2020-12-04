d94445472f337ecdbcc570311a400b13023c210a
kamcha7ka <sledsib@ya.ru>
2020-12-04 17:52:08 +0300

    Добавит реализацию формы загрузки изображения

--- js/form-upload.js
+++ js/form-upload.js
@@ -1,12 +1,33 @@
+import noUiSlider from "nouislider";
 import {KeyCode, toggleScrollElement} from "./util.js";
+
+import 'nouislider/distribute/nouislider.css';
 
 const bodyElement = document.querySelector(`body`);
 const uploadForm = bodyElement.querySelector(`.img-upload__form`);
 const uploadFormCloseButton = uploadForm.querySelector(`#upload-cancel`);
 const photoEditBlock = bodyElement.querySelector(`.img-upload__overlay`);
 const photoPreviewElement = photoEditBlock.querySelector(`.img-upload__preview img`);
+const effectLevelElement = photoEditBlock.querySelector(`.effect-level`);
+const effectSliderElement = effectLevelElement.querySelector(`.effect-slider`);
+const effectsList = photoEditBlock.querySelector(`.effects__list`);
 
 const FILE_TYPES = [`gif`, `jpg`, `jpeg`, `png`];
+const Slider = {
+  MIN_VALUE: 0,
+  MAX_VALUE: 100
+};
+const effect = {
+  chrome: (value) => `grayscale(${value / 100})`,
+  sepia: (value) => `sepia(${value / 100})`,
+  marvin: (value) => `invert(${value}%)`,
+  phobos: (value) => `blur(${value * 0.03}px)`,
+  heat: (value) => `brightness(${value * 0.02 + 1})`
+};
+const defaultEffect = `none`;
+
+let currentClass = ``;
+let slider;
 
 const compareTypeFile = (file) => {
   return FILE_TYPES.some((fileType) => file.name.toLowerCase().endsWith(fileType));
@@ -20,6 +41,63 @@ const setPhotoPreview = (file) => {
   });
 
   fileReader.readAsDataURL(file);
+};
+
+const initiateSlider = (element) => {
+  slider = noUiSlider.create(element, {
+    start: Slider.MAX_VALUE,
+    range: {
+      min: [Slider.MIN_VALUE],
+      max: [Slider.MAX_VALUE]
+    }
+  });
+};
+
+const resetEffectsForm = () => {
+  effectsList.querySelectorAll(`.effects__radio`).forEach((effectsInput) => {
+    effectsInput.checked = (effectsInput.value !== defaultEffect) ? false : true;
+  });
+};
+
+const removeCurrentClassForPhoto = () => {
+  if (currentClass !== ``) {
+    photoPreviewElement.classList.remove(currentClass);
+  }
+};
+
+const setEffectToPhoto = (nameEffect, value) => {
+  if (nameEffect !== defaultEffect) {
+    photoPreviewElement.style.filter = effect[nameEffect](value);
+  } else {
+    photoPreviewElement.style.filter = ``;
+  }
+};
+
+const currentEffectHandler = (evt) => {
+  evt.stopPropagation();
+  setCurrentEffect(evt.target.value);
+};
+
+const setCurrentEffect = (nameEffect) => {
+  if (nameEffect !== defaultEffect) {
+    removeCurrentClassForPhoto();
+    currentClass = `photo__preview--${nameEffect}`;
+
+    photoPreviewElement.classList.add(currentClass);
+
+    slider.off();
+    effectLevelElement.style.display = `block`;
+    slider.set([Slider.MAX_VALUE]);
+    slider.on(`update`, (values, handle) => {
+      setEffectToPhoto(nameEffect, values[handle]);
+    });
+  } else {
+    removeCurrentClassForPhoto();
+    setEffectToPhoto(nameEffect);
+    currentClass = ``;
+
+    effectLevelElement.style.display = `none`;
+  }
 };
 
 const loadModalClickCloseHandler = (evt) => {
@@ -36,6 +114,9 @@ const loadModalEscCloseHandler = (evt) => {
 const closeLoadModalForm = () => {
   photoEditBlock.classList.add(`hidden`);
   toggleScrollElement(`on`, bodyElement);
+
+  slider.destroy();
+  slider = null;
 
   uploadForm.reset();
 
@@ -56,6 +137,15 @@ const openLoadModalForm = (file) => {
 
     uploadFormCloseButton.addEventListener(`click`, loadModalClickCloseHandler);
     document.addEventListener(`keydown`, loadModalEscCloseHandler);
+
+    resetEffectsForm();
+    setEffectToPhoto(defaultEffect);
+
+    effectLevelElement.style.display = `none`;
+
+    initiateSlider(effectSliderElement);
+
+    effectsList.addEventListener(`change`, currentEffectHandler);
   } else {
     uploadForm.reset();
   }
